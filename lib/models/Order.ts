575f43f11bfd32ea4f09320ad76cf4f51e9462c6
sailtasya <romlahrom628@gmail.com>
2024-06-30 22:56:29 +0700

    chore: model on reference

--- lib/models/Order.ts
+++ lib/models/Order.ts
@@ -1,13 +1,18 @@
 import mongoose from "mongoose";
+import Product from "./Product";
 
 const orderSchema = new mongoose.Schema({
+  sessionId: String,
   customerClerkId: String,
   products: [
     {
       product: {
+        // Reference to the Product model
         type: mongoose.Schema.Types.ObjectId,
-        ref: "Product",
+        // 
+        ref: Product,
       },
+      productId: String,
       color: String,
       size: String,
       quantity: Number,
@@ -28,6 +33,7 @@ const orderSchema = new mongoose.Schema({
   },
 });
 
+// If the model already exists, use it; otherwise, create a new one
 const Order = mongoose.models.Order || mongoose.model("Order", orderSchema);
 
 export default Order;
